28b722fa5545abf3837f5bea1d2c0de9ab570958
missopi <missopi@me.com>
2022-09-17 09:06:20 +0100

    Complete exercise

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -3,6 +3,8 @@ def blank_seating_chart(number_of_rows, seats_per_row)
   # number_of_rows nested arrays, each with seats_per_row entries of nil to
   # represent that each seat is empty.
 
+  blank_seating_chart = Array.new(number_of_rows) { Array.new(seats_per_row) }
+
   # Example: blank_seating_chart(2, 3) should return:
   # [
   #   [nil, nil, nil],
@@ -16,16 +18,27 @@ end
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+
+  chart[row_index].push(seat_to_add)
+  chart
+
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+
+  chart.push(row_to_add)
+  chart
+
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
+  chart[row_index].delete_at(seat_index)
+
+  chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
@@ -33,10 +46,17 @@ end
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+
+  chart.delete_at(row_index)
+
+  chart
+
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
+
+  chart.flatten.count(nil)
 
   # NOTE: `chart` should **not** be mutated
 end
@@ -52,6 +72,17 @@ def find_favorite(array_of_hash_objects)
   #   { name: 'JavaScript', is_my_favorite?: false },
   #   { name: 'HTML', is_my_favorite?: false }
   # ]
+
+  array_of_hash_objects.each do |codes|
+    codes.each do |key, value|
+      if value == true
+        return codes
+      end
+    end
+  end
+
+  puts array_of_hash_objects
+
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
